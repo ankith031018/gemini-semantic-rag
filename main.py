eb765c512eafe93746f43dfdb4b4a1fc35a3ee52
ankith031018 <bhararadwajankith@gmail.com>
2026-01-10 22:07:35 +0530

    Definig functions, Just Structuring thins

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import os
 from dotenv import load_dotenv
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
 
-load_dotenv()  # Load environment variables from .env file
+load_dotenv()
 
-# Read the Google API key directly from the environment (populated by .env)
+
 google_api_key = os.getenv("GOOGLE_API_KEY")
 if not google_api_key:
     raise EnvironmentError("GOOGLE_API_KEY not found. Please add it to your .env file")
 
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
 embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
-vector = embeddings.embed_query("Hello world")
-print(vector[:5])  
+vector = embeddings.embed_query("Hello world") # Supposed make changes with Chunked documents
