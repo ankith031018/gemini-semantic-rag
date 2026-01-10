a47c873d9cb44bd7e91ba7ab33b8fffadcdcf100
ankith031018 <bhararadwajankith@gmail.com>
2026-01-10 23:59:46 +0530

    fix: update main script and dependency lockfiles

--- main.py
+++ main.py
@@ -2,14 +2,32 @@ import os
 from dotenv import load_dotenv
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from pinecone import Pinecone, ServerlessSpec
 
 load_dotenv()
-
 
 google_api_key = os.getenv("GOOGLE_API_KEY")
 if not google_api_key:
     raise EnvironmentError("GOOGLE_API_KEY not found. Please add it to your .env file")
 
+def Load_documents():
+    loader = PyPDFLoader(file_path="")
+    docs = loader.load()
+    return docs
 
-embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
-vector = embeddings.embed_query("Hello world") # Supposed make changes with Chunked documents
+def document_chunking(docs):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=200, add_start_index=True
+    )
+    splits = text_splitter.split_documents(docs)
+    return splits
+
+def get_embeddings(splits):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
+    vector_1 = embeddings.embed_query(splits[0].page_content)
+    vector_2 = embeddings.embed_query(splits[1].page_content)
+
+    assert len(vector_1) == len(vector_2)
+    print(f"Generated vectors of length {len(vector_1)}\n")
+    print(vector_1[:10])
